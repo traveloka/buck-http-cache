afc83d16ad3187b7edcf1d5ea2138ae9ebf545f5
Hendy Christianto <hendy.christianto@traveloka.com>
2020-09-22 23:29:33 +0700

    feat: use 2 GB RAM for jvm

--- run_buck_cache_client.sh
+++ run_buck_cache_client.sh
@@ -2,6 +2,4 @@
 
 ./gradlew distJar
 
-mkdir -p ./storage
-
-java -Xmx1g  -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:MaxDirectMemorySize=128m -Dlog.home=/var/log/buck-cache-client/logs -jar cache/build/libs/cache-1.0.0-standalone.jar server cache/src/dist/config/$1.yml
+java -Xmx2g  -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:MaxDirectMemorySize=128m -Dlog.home=/var/log/buck-cache-client/logs -jar cache/build/libs/cache-1.0.0-standalone.jar server cache/src/dist/config/$1.yml
